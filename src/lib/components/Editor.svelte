6ae0c8a89938af9ca3afd1d04255ab888e38f38a
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-07-03 18:17:36 -0400

    add autocomplete to editor component

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-    import { basicEditor } from "prism-code-editor/setups";
+    import { createEditor } from "prism-code-editor";
+    import { editorCommands, defaultKeymap } from "prism-code-editor/commands";
     import "prism-code-editor/prism/languages/javascript";
     import type { PrismEditor } from "prism-code-editor";
+
+    import "prism-code-editor/themes/atom-one-dark.css";
+    import "prism-code-editor/layout.css";
+    import "prism-code-editor/scrollbar.css";
+    import "prism-code-editor/autocomplete.css";
+    import "prism-code-editor/autocomplete-icons.css";
 
     import { onMount } from "svelte";
 
@@ -15,13 +22,50 @@
     let editorElement: HTMLDivElement;
     let editor: PrismEditor;
 
-    onMount(() => {
-        editor = basicEditor(editorElement, {
+    onMount(async () => {
+        // These imports throw errors when rendered in SSR, so we import them
+        // lazily on mount.
+        const {
+            strictFilter,
+            autoComplete,
+            registerCompletions,
+            completeFromList,
+        } = await import("prism-code-editor/autocomplete");
+        const { cursorPosition } = await import("prism-code-editor/cursor");
+        const {
+            completeKeywords,
+            jsDocCompletion,
+            jsSnipets,
+            jsContext,
+            jsCompletion,
+        } = await import("prism-code-editor/autocomplete/javascript");
+        const { searchWidget } = await import("prism-code-editor/search");
+
+        registerCompletions(
+            ["javascript", "js", "jsx", "tsx", "typescript", "ts"],
+            {
+                context: jsContext,
+                sources: [
+                    jsCompletion(window),
+                    completeKeywords,
+                    jsDocCompletion,
+                    completeFromList(jsSnipets),
+                ],
+            },
+        );
+
+        editor = createEditor(editorElement, {
             language: "js",
-            theme: "atom-one-dark",
             onUpdate,
             value: initialValue,
         });
+
+        editor.addExtensions(
+            editorCommands(defaultKeymap),
+            cursorPosition(),
+            searchWidget(),
+            autoComplete({ filter: strictFilter }),
+        );
     });
 </script>
 
