34f952fb0e357f0f2acd246f4ef805b81ac399f3
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-07-03 23:48:18 -0400

    add level interface

---
+++ src/lib/levels/index.ts
@@ -0,0 +1,45 @@
+export interface TestResult {
+  passed: boolean;
+  msg: string;
+}
+
+export interface Level {
+  /**
+   * The name of the level.
+   *
+   * @example "Array Summing"
+   */
+  name: string;
+
+  /**
+   * The string of JS to inject in the VM that provides the inputs for the
+   * player to process.
+   *
+   * IMPORTANT: Remember to include `const input = ` and **end every statement
+   * with a semicolon**.
+   *
+   * @example
+   * ```js
+   * const input = [1, 2, 3, 4, 5];
+   * ```
+   */
+  input: string;
+
+  /**
+   * The string of JS to use as the initial value. It should pass the `test`
+   * function, but it should fail FP linting in as many ways as possible.
+   *
+   * @example
+   * ```js
+   * return input.reduce((acc, num) => acc + num, 0);
+   * ````
+   */
+  initialCode: string;
+
+  /**
+   * A function that inputs the user's code and runs it against a suite of
+   * tests to ensure that it works. Make sure to generalize the input to ensure
+   * that the user didn't just hardcode the answer.
+   */
+  test: (userCode: string) => TestResult;
+}
