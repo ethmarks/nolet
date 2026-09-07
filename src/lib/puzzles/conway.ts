44a9239d49fc92d27a93f8d470681f2aa163846b
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-06 20:51:23 -0400

    add comments to starter code in conway puzzle

--- src/lib/puzzles/conway.ts
+++ src/lib/puzzles/conway.ts
@@ -100,6 +100,32 @@ const input = ${JSON.stringify(stringToCells(this.secretInput))};
 const steps = ${this.secretSteps};`;
 
 	public initialCode: string = js`
+// Lemme briefly explain the algorithm in this starter code,
+// because it's a bit unintuitive.
+//
+// Rather than tracking each position in the grid and whether
+// that cell is alive or dead, it uses an array to track all
+// the live cells and their positions. If a position isn't in
+// the array of live cells, it must be a dead cell. This
+// approach makes things way easier because I don't have to
+// worry about storing a dynamic-size grid or dealing with
+// behavior at the grid edges.
+//
+// The rules say to iterate over *every* cell, but that'd be a
+// lot of unecessary work because I only care about live
+// cells, and there's no way for dead cells to become alive if
+// they aren't neighboring living cells. So I optimize it by
+// only iterating over the neighbors of living cells. This
+// excludes dead cells that aren't adjacent to any live cells,
+// and it also excludes lone live cells.
+//
+// For each neighbor of a living cell, it counts how many
+// neighbors it has and decide whether it should be alive in
+// the next generation (logic explained in lines 65-74).
+//
+// I think that's basically it. Good luck!
+//
+
 const MOORE_NEIGHBORHOOD = [
 	[-1, -1], [0, -1], [1, -1],
 	[-1,  0],          [1,  0],
@@ -108,8 +134,10 @@ const MOORE_NEIGHBORHOOD = [
 
 function simulate(cells, steps) {
 	for (let i = 0; i < steps; i++) {
+		// This step generates a Record where the keys are every
+		// position that is a neighbor of a living cell, and the
+		// values are how many living cells it's the neighbor of.
 		const neighborCounts = {};
-
 		for (const { x, y } of cells) {
 			for (const [dx, dy] of MOORE_NEIGHBORHOOD) {
 				// string key to simplify things because using
@@ -120,13 +148,32 @@ function simulate(cells, steps) {
 			}
 		}
 
+		// This step uses the neighborCounts to generate a new
+		// array of live cells.
 		const nextCells = [];
-
 		for (const key in neighborCounts) {
 			const count = neighborCounts[key];
+
+			// This just undoes the key-constructing step that I did
+			// in the first step.
 			const [x, y] = key.split(",").map(Number);
+
+			// This check is really really inefficient, and honestly
+			// I should probably use a lookup table instead. But
+			// performance doesn't really matter and I wanted to
+			// keep it simple.
 			const alive = cells.some((c) => c.x === x && c.y === y);
 
+			// This is just a rephrasing of the CGoL rules.
+			//
+			// If count is 3, then the cell should live regardless
+			// of whether it's currently alive.
+			//
+			// If the count is 2, the cell should only live if it's
+			// currently alive.
+			//
+			// In all other scenarios, the cell should either die
+			// or stay dead.
 			if (count === 3 || (alive && count === 2)) {
 				nextCells.push({ x, y });
 			}
@@ -157,15 +204,6 @@ function simulate(cells, steps) {
 	// This step generates an array of every position that
 	// neighbors a living cell.
 	//
-	// I can get away with only considering the neighbors,
-	// because a cell can only ever be alive if it's a neighbor
-	// of a live cell.
-	//
-	// And I can get away with not considering the current live
-	// cells themselves, because if a live cell isn't the
-	// neighbor of any cells, it'll die in the next generation
-	// and I don't need to check its neighbor count.
-	//
 	// There can and should be duplicate entries. If a
 	// position is the neighbor of two living cells, it'll
 	// appear two times in the array. More about that in the
@@ -178,11 +216,11 @@ function simulate(cells, steps) {
 	// This step converts the duplicate-having neighbor array
 	// into a deduplicated Record<position, count>.
 	//
-	// This is the reason that in the previous step, I
-	// converted the x and y positions into a string rather than
-	// just doing [x+dx, y+dx] or something like that. In JS,
-	// strings are passed by value, but arrays are passed by
-	// reference, which means I can't really use them as keys.
+	// This is why in the previous step, I converted the x and
+	// y positions into a string rather than just doing
+	// [x+dx, y+dx] or something like that. In JS, strings are
+	// passed by value, but arrays are passed by reference,
+	// which means I can't really use them as keys.
 	const neighborCounts = neighbors.reduce((acc, key) => (
 		{ ...acc, [key]: (acc[key] ?? 0) + 1 }
 	), {});
@@ -195,26 +233,9 @@ function simulate(cells, steps) {
 	// couldn't use a for...in loop (because of the linter).
 	const nextCells = Object.entries(neighborCounts).reduce(
 		(acc, [key, count]) => {
-			// This just undoes the key-constructing step that I did
-			// in the first step.
 			const [x, y] = key.split(",").map(Number);
-
-			// This check is really really inefficient, and honestly
-			// I should probably use a lookup table instead. But
-			// performance doesn't really matter and I want to keep
-			// it simple.
 			const alive = cells.some((c) => c.x === x && c.y === y);
 
-			// This is just a rephrasing of the CGoL rules.
-			//
-			// If count is 3, then the cell should live regardless
-			// of whether it's currently alive.
-			//
-			// If the count is 2, the cell should only live if it's
-			// currently alive.
-			//
-			// In all other scenarios, the cell should either die
-			// or stay dead.
 			if (count === 3 || (alive && count === 2)) {
 				return [...acc, { x, y }];
 			}
